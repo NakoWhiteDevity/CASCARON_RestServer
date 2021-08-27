fbe6cf8e89fff66f005861ac6ac372c8306e84ff
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-08-27 19:20:10 +0200

    cascaron para la tarea del 171

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ const Categoria = require('./categoria');
 const Role = require('./role');
 const Server = require('./server');
 const Usuario = require('./usuario');
+const Producto = require('./producto');
 
-module.exports = { Categoria , Role , Server , Usuario }
+module.exports = { Categoria , Role , Server , Usuario , Producto }
--- models/server.js
+++ models/server.js
@@ -11,6 +11,7 @@ class Server {
             usuarios:   '/api/usuario',
             auth:       '/api/auth',
             categorias: '/api/categorias',
+            productos:  '/api/productos'
         }
         this.conectarDB();
         this.middlewares();
@@ -33,6 +34,7 @@ class Server {
         this.app.use(this.paths.usuarios,require('../routes/user.routes'))
         this.app.use(this.paths.auth,require('../routes/auth.routes'));
         this.app.use(this.paths.categorias,require('../routes/categorias.routes'));
+        this.app.use(this.paths.productos,require('../routes/productos.routes'));
     }
 
     async conectarDB(){
