60b0560a0193b254ee11809c223e70d4d52c791b
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-09-08 17:38:53 +0200

    174

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,10 +1,47 @@
 const { response } = require('express');
+const cPermitidas = ['roles','productos','categorias','usuarios'];
+const { ObjectId } = require('mongoose').Types;
+const { Usuario,Categoria,Producto } = require('../models');
+
+const buscarUsuarios = async(termino = '',res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino);
+    if(esMongoID){
+        const usuario = await Usuario.findById(termino);
+        res.status(200).json({
+            resultados : (usuario) ? [usuario] : []
+        })
+    }
+
+}
 
 const buscar = (req,res = response) => {
     
     const { coleccion , termino } = req.params;
 
-    res.json({msg:'buscar...'})
+    if( !cPermitidas.includes(coleccion) ){
+        return res.status(400).json({msg:`Las colecciones permitidas son : ${cPermitidas}`});
+    }
+
+    switch(coleccion){
+        case 'roles':
+            
+        break;
+        case 'productos':
+            
+        break;
+        case 'categorias':
+            
+        break;
+        case 'usuarios':
+            buscarUsuarios(termino,res);
+        break;
+        default:
+            res.status(500).json({msg:'Se me fue de madre, mi rey.'});
+        break;
+    }
+
+    //res.status(400).json({coleccion,termino});
 
 }
 
